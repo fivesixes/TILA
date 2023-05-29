b89ea37ec14c6d4afe5b0bc2c8b5f69ec8dc8945
Dera <deraegbo@gmail.com>
2023-05-29 20:17:20 +0100

    Setting up the repo

---
+++ app.py
@@ -0,0 +1,2 @@
+print('Hi dad')
+_in = input()
---
+++ run.ps1
@@ -0,0 +1,10 @@
+$interpreter = "C:\Python311\python.exe"
+$scriptPath = "app.py"
+
+$command = "$interpreter $scriptPath"
+
+Write-Host "Running script $scriptPath"
+Write-Host "--------------------------"
+Invoke-Expression -Command $command
+Write-Host "--------------------------"
+Write-Host "$scriptPath closed"
